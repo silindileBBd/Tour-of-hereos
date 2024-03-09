67eb756ef6ffb1863ea194f3eb95c2095dfd82eb
Silindile Madlala <162616496+silindileBBd@users.noreply.github.com>
2024-03-09 19:23:34 +0200

    import heroDetailComponent

--- angular-tour-of-heroes/src/app/components/heroes/heroes.component.ts
+++ angular-tour-of-heroes/src/app/components/heroes/heroes.component.ts
@@ -3,11 +3,19 @@ import { CommonModule, NgFor, NgIf, UpperCasePipe } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { Hero } from '../../hero';
 import { HEROES } from '../../mock-heroes';
+import { HeroDetailComponent } from '../hero-detail/hero-detail.component';
 
 @Component({
   selector: 'app-heroes',
   standalone: true,
-  imports: [CommonModule, FormsModule, NgFor, NgIf, UpperCasePipe],
+  imports: [
+    CommonModule,
+    FormsModule,
+    NgFor,
+    NgIf,
+    UpperCasePipe,
+    HeroDetailComponent,
+  ],
   templateUrl: './heroes.component.html',
   styleUrl: './heroes.component.css',
 })
