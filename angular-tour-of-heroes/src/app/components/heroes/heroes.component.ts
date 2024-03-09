3e3e009d95408fcdb884ed75cf53eeb444d36e10
Silindile Madlala <162616496+silindileBBd@users.noreply.github.com>
2024-03-09 18:21:57 +0200

    import commonModule

--- angular-tour-of-heroes/src/app/components/heroes/heroes.component.ts
+++ angular-tour-of-heroes/src/app/components/heroes/heroes.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
 import { Hero } from '../../hero';
 
 @Component({
   selector: 'app-heroes',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './heroes.component.html',
   styleUrl: './heroes.component.css',
 })
