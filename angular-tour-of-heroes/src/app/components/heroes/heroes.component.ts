b7a3f44ba93371b1594304de10bb6a512c1bd5b1
Silindile Madlala <162616496+silindileBBd@users.noreply.github.com>
2024-03-09 19:00:47 +0200

    creates onSelect component

--- angular-tour-of-heroes/src/app/components/heroes/heroes.component.ts
+++ angular-tour-of-heroes/src/app/components/heroes/heroes.component.ts
@@ -13,4 +13,9 @@ import { HEROES } from '../../mock-heroes';
 })
 export class HeroesComponent {
   heroes = HEROES;
+
+  selectedHero?: Hero;
+  onSelect(hero: Hero): void {
+    this.selectedHero = hero;
+  }
 }
