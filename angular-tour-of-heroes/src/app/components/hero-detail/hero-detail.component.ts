2795c1dbb65b2cf345645f15cac7762c5f3f2046
Silindile Madlala <162616496+silindileBBd@users.noreply.github.com>
2024-03-09 19:22:56 +0200

    import Hero and Input

--- angular-tour-of-heroes/src/app/components/hero-detail/hero-detail.component.ts
+++ angular-tour-of-heroes/src/app/components/hero-detail/hero-detail.component.ts
@@ -1,12 +1,13 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { Hero } from '../../hero';
 
 @Component({
   selector: 'app-hero-detail',
   standalone: true,
   imports: [],
   templateUrl: './hero-detail.component.html',
-  styleUrl: './hero-detail.component.css'
+  styleUrl: './hero-detail.component.css',
 })
 export class HeroDetailComponent {
-
+  @Input() hero?: Hero;
 }
