cbbb874c0a43224560c9ca7cf84d2b5a7dcb3276
Silindile Madlala <162616496+silindileBBd@users.noreply.github.com>
2024-03-09 19:13:24 +0200

    import NgIf and UpperCasePipe

--- angular-tour-of-heroes/src/app/components/heroes/heroes.component.ts
+++ angular-tour-of-heroes/src/app/components/heroes/heroes.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { CommonModule, NgFor } from '@angular/common';
+import { CommonModule, NgFor, NgIf, UpperCasePipe } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { Hero } from '../../hero';
 import { HEROES } from '../../mock-heroes';
@@ -7,7 +7,7 @@ import { HEROES } from '../../mock-heroes';
 @Component({
   selector: 'app-heroes',
   standalone: true,
-  imports: [CommonModule, FormsModule, NgFor],
+  imports: [CommonModule, FormsModule, NgFor, NgIf, UpperCasePipe],
   templateUrl: './heroes.component.html',
   styleUrl: './heroes.component.css',
 })
