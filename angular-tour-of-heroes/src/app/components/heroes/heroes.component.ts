2592b6315486453f90c0ca01a8849f17c630ad2a
Silindile Madlala <162616496+silindileBBd@users.noreply.github.com>
2024-03-09 18:26:01 +0200

    import FormModule

--- angular-tour-of-heroes/src/app/components/heroes/heroes.component.ts
+++ angular-tour-of-heroes/src/app/components/heroes/heroes.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 import { Hero } from '../../hero';
 
 @Component({
   selector: 'app-heroes',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, FormsModule],
   templateUrl: './heroes.component.html',
   styleUrl: './heroes.component.css',
 })
