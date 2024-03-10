a3bdedb32afad0a9e978f7d6ac4892bcc5873f1b
Silindile Madlala <162616496+silindileBBd@users.noreply.github.com>
2024-03-10 04:15:18 +0200

    removed unused variables

--- angular-tour-of-heroes/src/app/components/hero-detail/hero-detail.component.ts
+++ angular-tour-of-heroes/src/app/components/hero-detail/hero-detail.component.ts
@@ -1,10 +1,12 @@
 import { Component, Input } from '@angular/core';
+import { NgIf, UpperCasePipe } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 import { Hero } from '../../hero';
 
 @Component({
   selector: 'app-hero-detail',
   standalone: true,
-  imports: [],
+  imports: [NgIf, UpperCasePipe, FormsModule],
   templateUrl: './hero-detail.component.html',
   styleUrl: './hero-detail.component.css',
 })
