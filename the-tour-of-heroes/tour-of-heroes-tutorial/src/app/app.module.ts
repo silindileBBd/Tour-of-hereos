fd18602b2ebb31b23a3d6e5e739a85436e7037a0
Silindile Madlala <162616496+silindileBBd@users.noreply.github.com>
2024-03-10 19:07:00 +0200

    fixed spelling error

--- the-tour-of-heroes/tour-of-heroes-tutorial/src/app/app.module.ts
+++ the-tour-of-heroes/tour-of-heroes-tutorial/src/app/app.module.ts
@@ -5,10 +5,10 @@ import { FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeroesComponent } from './components/heroes/heroes.component';
-import { HeroeDetailComponent } from './components/heroe-detail/heroe-detail.component';
+import { HeroDetailComponent } from './components/hero-detail/hero-detail.component';
 
 @NgModule({
-  declarations: [AppComponent, HeroesComponent, HeroeDetailComponent],
+  declarations: [AppComponent, HeroesComponent, HeroDetailComponent],
   imports: [BrowserModule, AppRoutingModule, FormsModule],
   providers: [],
   bootstrap: [AppComponent],
