7a012afd81b7d5330a153d59d72b26ffca561dab
Silindile Madlala <162616496+silindileBBd@users.noreply.github.com>
2024-03-10 04:28:43 +0200

    added getHeroes  and ngInit methoods

--- angular-tour-of-heroes/src/app/components/heroes/heroes.component.ts
+++ angular-tour-of-heroes/src/app/components/heroes/heroes.component.ts
@@ -16,6 +16,14 @@ export class HeroesComponent {
   heroes: Hero[] = [];
   constructor(private heroService: HeroService) {}
 
+  getHeroes(): void {
+    this.heroService.getHeroes().subscribe((heroes) => (this.heroes = heroes));
+  }
+
+  ngOnInit(): void {
+    this.getHeroes();
+  }
+
   selectedHero?: Hero;
   onSelect(hero: Hero): void {
     this.selectedHero = hero;
