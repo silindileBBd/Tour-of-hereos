4adb31a8d09402d41595c5dbfe63dd5187a14913
Silindile Madlala <162616496+silindileBBd@users.noreply.github.com>
2024-03-10 18:21:03 +0200

    import an interface

--- the-tour-of-heroes/tour-of-heroes-tutorial/src/app/components/heroes/heroes.component.ts
+++ the-tour-of-heroes/tour-of-heroes-tutorial/src/app/components/heroes/heroes.component.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
-
+import { Hero } from '../../hero';
 @Component({
   selector: 'app-heroes',
   templateUrl: './heroes.component.html',
   styleUrl: './heroes.component.css',
 })
 export class HeroesComponent {
-  hero = 'Windstorm';
+  hero: Hero = {
+    id: 1,
+    name: 'Windstorm',
+  };
 }
