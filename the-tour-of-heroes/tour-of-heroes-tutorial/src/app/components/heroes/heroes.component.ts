05c9ef40d8be407d9178b696b8bb133733927fe6
Silindile Madlala <162616496+silindileBBd@users.noreply.github.com>
2024-03-10 20:59:38 +0200

    added delete method

--- the-tour-of-heroes/tour-of-heroes-tutorial/src/app/components/heroes/heroes.component.ts
+++ the-tour-of-heroes/tour-of-heroes-tutorial/src/app/components/heroes/heroes.component.ts
@@ -20,4 +20,9 @@ export class HeroesComponent implements OnInit {
   getHeroes(): void {
     this.heroService.getHeroes().subscribe((heroes) => (this.heroes = heroes));
   }
+
+  delete(hero: Hero): void {
+    this.heroes = this.heroes.filter((h) => h !== hero);
+    this.heroService.deleteHero(hero.id).subscribe();
+  }
 }
