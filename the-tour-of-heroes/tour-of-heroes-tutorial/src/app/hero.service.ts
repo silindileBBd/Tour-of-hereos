c2b97d7b16e87c5fe5d83f04f474e130fd568152
Silindile Madlala <162616496+silindileBBd@users.noreply.github.com>
2024-03-10 21:01:31 +0200

    added deleteHero method

--- the-tour-of-heroes/tour-of-heroes-tutorial/src/app/hero.service.ts
+++ the-tour-of-heroes/tour-of-heroes-tutorial/src/app/hero.service.ts
@@ -59,4 +59,13 @@ export class HeroService {
   private log(message: string) {
     this.messageService.add(`HeroService: ${message}`);
   }
+
+  deleteHero(id: number): Observable<Hero> {
+    const url = `${this.heroesUrl}/${id}`;
+
+    return this.http.delete<Hero>(url, this.httpOptions).pipe(
+      tap((_) => this.log(`deleted hero id=${id}`)),
+      catchError(this.handleError<Hero>('deleteHero'))
+    );
+  }
 }
